cbefcb72924ef803e820849d07fe95201278372f
Waqar Khan <waqar.mrwt424@gmail.com>
2025-04-20 21:42:13 +0500

    sitemap done

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+
+
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4332720109955132"
+          crossOrigin="anonymous"></script>
+      </head>
+
+
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
