0a10cfd9d6e499550111d7c454322ef9b6f51626
Waqar Khan <waqar.mrwt424@gmail.com>
2025-04-20 13:23:23 +0500

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // <-- add this block
+  {
+    // disable “no-explicit-any” and “no-unused-vars”
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+
+  // tell Next.js to run the build even if ESLint finds errors\
+
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+
+
+
 };
 
 export default nextConfig;
