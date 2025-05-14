13115039640672163b0713a651669e233d316de1
Waqar Khan <waqar.mrwt424@gmail.com>
2025-05-14 21:12:14 +0500

    final commit

--- lib/ministries.ts
+++ lib/ministries.ts
@@ -20,6 +20,8 @@ export type Ministry = {
 }
 
 export const ministries: Ministry[] = [
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     {
         name: "Ministry of Interior",
         slug: "interior",
@@ -135,4 +137,158 @@ export const ministries: Ministry[] = [
             },
         ],
     },
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // ADD just below the Interior ministry object
+    {
+        name: "Ministry of Defence",
+        slug: "defence",
+        departments: [],
+    },
+    {
+        name: "Ministry of Finance & Revenue",
+        slug: "finance",
+        departments: [],
+    },
+    {
+        name: "Ministry of Foreign Affairs",
+        slug: "foreign-affairs",
+        departments: [],
+    },
+    {
+        name: "Ministry of Federal Education & Professional Training",
+        slug: "federal-education",
+        departments: [],
+    },
+    {
+        name: "Ministry of Information & Broadcasting",
+        slug: "information-broadcasting",
+        departments: [],
+    },
+    {
+        name: "Ministry of Information Technology & Telecommunication",
+        slug: "it-telecom",
+        departments: [],
+    },
+    {
+        name: "Ministry of Climate Change",
+        slug: "climate-change",
+        departments: [],
+    },
+    {
+        name: "Ministry of Commerce",
+        slug: "commerce",
+        departments: [],
+    },
+    {
+        name: "Ministry of Communications",
+        slug: "communications",
+        departments: [],
+    },
+    {
+        name: "Ministry of Defence Production",
+        slug: "defence-production",
+        departments: [],
+    },
+    {
+        name: "Ministry of Economic Affairs",
+        slug: "economic-affairs",
+        departments: [],
+    },
+    {
+        name: "Ministry of Energy",
+        slug: "energy",
+        departments: [],
+    },
+    {
+        name: "Ministry of Housing & Works",
+        slug: "housing-works",
+        departments: [],
+    },
+    {
+        name: "Ministry of Human Rights",
+        slug: "human-rights",
+        departments: [],
+    },
+    {
+        name: "Ministry of Industries & Production",
+        slug: "industries-production",
+        departments: [],
+    },
+    {
+        name: "Ministry of Kashmir Affairs & Gilgit-Baltistan",
+        slug: "kashmir-gb",
+        departments: [],
+    },
+    {
+        name: "Ministry of Law & Justice",
+        slug: "law-justice",
+        departments: [],
+    },
+    {
+        name: "Ministry of Maritime Affairs",
+        slug: "maritime-affairs",
+        departments: [],
+    },
+    {
+        name: "Ministry of Narcotics Control",
+        slug: "narcotics-control",
+        departments: [],
+    },
+    {
+        name: "Ministry of National Food Security & Research",
+        slug: "food-security",
+        departments: [],
+    },
+    {
+        name: "Ministry of National Health Services, Regulations & Coordination",
+        slug: "health-services",
+        departments: [],
+    },
+    {
+        name: "Ministry of Overseas Pakistanis & Human Resource Development",
+        slug: "overseas-hrd",
+        departments: [],
+    },
+    {
+        name: "Ministry of Parliamentary Affairs",
+        slug: "parliamentary-affairs",
+        departments: [],
+    },
+    {
+        name: "Ministry of Planning, Development & Special Initiatives",
+        slug: "planning-development",
+        departments: [],
+    },
+    {
+        name: "Ministry of Privatisation",
+        slug: "privatisation",
+        departments: [],
+    },
+    {
+        name: "Ministry of Railways",
+        slug: "railways",
+        departments: [],
+    },
+    {
+        name: "Ministry of Religious Affairs & Inter‑faith Harmony",
+        slug: "religious-affairs",
+        departments: [],
+    },
+    {
+        name: "Ministry of Science & Technology",
+        slug: "science-tech",
+        departments: [],
+    },
+    {
+        name: "Ministry of States & Frontier Regions",
+        slug: "safar",
+        departments: [],
+    },
+    {
+        name: "Ministry of Water Resources",
+        slug: "water-resources",
+        departments: [],
+    },
+
+
 ]
