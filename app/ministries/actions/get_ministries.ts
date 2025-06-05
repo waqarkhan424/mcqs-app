2c8017a399c7aaaa820a3084830fafa1213f0afd
Waqar Khan <waqar.mrwt424@gmail.com>
2025-06-05 17:21:40 +0500

    new commit

---
+++ app/ministries/actions/get_ministries.ts
@@ -0,0 +1,14 @@
+//@ts-nocheck
+import prisma from "@/lib/prisma";
+
+export async function get_ministries() {
+    return await prisma.ministry.findMany({
+        include: {
+            departments: {
+                include: {
+                    posts: true,
+                },
+            },
+        },
+    });
+}
--- app/ministries/page.tsx
+++ app/ministries/page.tsx
@@ -1,6 +1,63 @@
 
-import MinistriesLinks from "@/app/components/ministries-links";
+// import MinistriesLinks from "@/app/components/ministries-links";
 
-export default function MinistriesPage() {
-    return <MinistriesLinks />;
+// export default function MinistriesPage() {
+//     return <MinistriesLinks />;
+// }
+
+// "use client"
+
+import { Card, CardContent } from "@/components/ui/card"
+import { ministryData } from "@/lib/ministry-icons"
+import Typography from "@/components/ui/typography"
+// import { ministries } from "@/lib/ministries"
+import Link from "next/link"
+import { get_ministries } from "./actions/get_ministries"
+
+
+export default async function MinistriesPage() {
+
+
+    const ministries = await get_ministries();
+
+    return (
+        <div className="max-w-3xl mx-auto px-4 py-8">
+            <Typography variant="h2" className="text-center">
+                Ministries of Pakistan
+            </Typography>
+
+
+            <Typography variant="p" className="text-muted-foreground text-center mt-2 mb-6">
+                Select a ministry to explore departments and posts.
+            </Typography>
+
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+
+                {ministries.map((m) => {
+
+                    const { icon: Icon, color } =
+                        ministryData[m.slug] || { icon: ministryData["foreign-affairs"].icon, color: "text-gray-500" };
+
+                    return (
+                        <Card key={m.slug} className="bg-green-50 transition rounded-xl border-none shadow-sm"
+                        >
+                            <CardContent className="p-5">
+                                <Link
+                                    href={`/ministries/${m.slug}`}
+                                    className="flex items-center gap-3 font-medium text-blue-600 underline underline-offset-2"
+                                >
+                                    <Icon className={`w-5 h-5 ${color}`} />
+                                    {m.name}
+                                </Link>
+                            </CardContent>
+                        </Card>
+
+                    )
+                })}
+
+
+            </div>
+        </div>
+    )
 }
