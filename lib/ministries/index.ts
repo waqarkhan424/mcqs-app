fd3bff5fdf5d8e8afce3c7e8f2b954d10145fb8b
Waqar Khan <waqar.mrwt424@gmail.com>
2025-06-05 16:26:55 +0500

    new commit

--- lib/ministries/index.ts
+++ lib/ministries/index.ts
@@ -1,38 +1,107 @@
 // export * from "./types";
+
+// import { interior } from "./interior";
+// import { defence } from "./defence";
+// import { finance } from "./finance";
+// import { foreignAffairs } from "./foreign-affairs";
+// import { federalEducation } from "./federal-education";
+// import { informationBroadcasting } from "./information-broadcasting";
+// import { itTelecom } from "./it-telecom";
+// import { climateChange } from "./climate-change";
+// import { commerce } from "./commerce";
+// import { communications } from "./communications";
+// import { defenceProduction } from "./defence-production";
+// import { economicAffairs } from "./economic-affairs";
+// import { energy } from "./energy";
+// import { housingWorks } from "./housing-works";
+// import { humanRights } from "./human-rights";
+// import { industriesProduction } from "./industries-production";
+// import { kashmirGb } from "./kashmir-gb";
+// import { lawJustice } from "./law-justice";
+// import { maritimeAffairs } from "./maritime-affairs";
+// import { narcoticsControl } from "./narcotics-control";
+// import { foodSecurity } from "./food-security";
+// import { healthServices } from "./health-services";
+// import { overseasHrd } from "./overseas-hrd";
+// import { parliamentaryAffairs } from "./parliamentary-affairs";
+// import { planningDevelopment } from "./planning-development";
+// import { privatisation } from "./privatisation";
+// import { railways } from "./railways";
+// import { religiousAffairs } from "./religious-affairs";
+// import { scienceTech } from "./science-tech";
+// import { statesFrontierRegions } from "./states-frontier-regions";
+// import { waterResources } from "./water-resources";
+
+// export const ministries = [
+//     interior,
+//     defence,
+//     finance,
+//     foreignAffairs,
+//     federalEducation,
+//     informationBroadcasting,
+//     itTelecom,
+//     climateChange,
+//     commerce,
+//     communications,
+//     defenceProduction,
+//     economicAffairs,
+//     energy,
+//     housingWorks,
+//     humanRights,
+//     industriesProduction,
+//     kashmirGb,
+//     lawJustice,
+//     maritimeAffairs,
+//     narcoticsControl,
+//     foodSecurity,
+//     healthServices,
+//     overseasHrd,
+//     parliamentaryAffairs,
+//     planningDevelopment,
+//     privatisation,
+//     railways,
+//     religiousAffairs,
+//     scienceTech,
+//     statesFrontierRegions,
+//     waterResources,
+// ];
+
+
+
 //@ts-nocheck
 export * from "./types.ts";
 
-import { interior } from "./interior";
-import { defence } from "./defence";
-import { finance } from "./finance";
-import { foreignAffairs } from "./foreign-affairs";
-import { federalEducation } from "./federal-education";
-import { informationBroadcasting } from "./information-broadcasting";
-import { itTelecom } from "./it-telecom";
-import { climateChange } from "./climate-change";
-import { commerce } from "./commerce";
-import { communications } from "./communications";
-import { defenceProduction } from "./defence-production";
-import { economicAffairs } from "./economic-affairs";
-import { energy } from "./energy";
-import { housingWorks } from "./housing-works";
-import { humanRights } from "./human-rights";
-import { industriesProduction } from "./industries-production";
-import { kashmirGb } from "./kashmir-gb";
-import { lawJustice } from "./law-justice";
-import { maritimeAffairs } from "./maritime-affairs";
-import { narcoticsControl } from "./narcotics-control";
-import { foodSecurity } from "./food-security";
-import { healthServices } from "./health-services";
-import { overseasHrd } from "./overseas-hrd";
-import { parliamentaryAffairs } from "./parliamentary-affairs";
-import { planningDevelopment } from "./planning-development";
-import { privatisation } from "./privatisation";
-import { railways } from "./railways";
-import { religiousAffairs } from "./religious-affairs";
-import { scienceTech } from "./science-tech";
-import { statesFrontierRegions } from "./states-frontier-regions";
-import { waterResources } from "./water-resources";
+import { interior } from "./interior.ts";
+import { defence } from "./defence.ts";
+import { finance } from "./finance.ts";
+import { foreignAffairs } from "./foreign-affairs.ts";
+import { federalEducation } from "./federal-education.ts";
+import { informationBroadcasting } from "./information-broadcasting.ts";
+import { itTelecom } from "./it-telecom.ts";
+import { climateChange } from "./climate-change.ts";
+import { commerce } from "./commerce.ts";
+import { communications } from "./communications.ts";
+import { defenceProduction } from "./defence-production.ts";
+import { economicAffairs } from "./economic-affairs.ts";
+import { energy } from "./energy.ts";
+import { housingWorks } from "./housing-works.ts";
+import { humanRights } from "./human-rights.ts";
+import { industriesProduction } from "./industries-production.ts";
+import { kashmirGb } from "./kashmir-gb.ts";
+import { lawJustice } from "./law-justice.ts";
+import { maritimeAffairs } from "./maritime-affairs.ts";
+import { narcoticsControl } from "./narcotics-control.ts";
+import { foodSecurity } from "./food-security.ts";
+import { healthServices } from "./health-services.ts";
+import { overseasHrd } from "./overseas-hrd.ts";
+import { parliamentaryAffairs } from "./parliamentary-affairs.ts";
+import { planningDevelopment } from "./planning-development.ts";
+import { privatisation } from "./privatisation.ts";
+import { railways } from "./railways.ts";
+import { religiousAffairs } from "./religious-affairs.ts";
+import { scienceTech } from "./science-tech.ts";
+import { statesFrontierRegions } from "./states-frontier-regions.ts";
+import { waterResources } from "./water-resources.ts";
 
 export const ministries = [
     interior,
