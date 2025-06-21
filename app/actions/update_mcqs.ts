53d0c953f426643c3250bcea5c9851b0b37b0839
Waqar Khan <waqar.mrwt424@gmail.com>
2025-06-21 22:26:58 +0500

    new commit

--- app/actions/update_mcqs.ts
+++ app/actions/update_mcqs.ts
@@ -2,7 +2,7 @@
 
 import prisma from "@/lib/prisma";
 
-export async function update_mcqs(id: string, data: { question: string; options: string[]; correctAnswer: string; explanation?: string; }) {
+export async function update_mcqs(id: string, data: { question: string; options: string[]; correctAnswer: string }) {
     try {
         await prisma.question.update({
             where: { id },
